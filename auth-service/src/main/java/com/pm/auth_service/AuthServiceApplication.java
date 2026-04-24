22ad26594f3c174a58b1c87a5cfb366166c74d87
benpointdex <185585972+benpointdex@users.noreply.github.com>
2026-04-24 20:43:04 +0530

    Global fix:  Minor auth update and gitignore maintenance

--- auth-service/src/main/java/com/pm/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/pm/auth_service/AuthServiceApplication.java
@@ -2,10 +2,11 @@ package com.pm.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EntityScan("com.pm.auth_service.model")
 @EnableJpaRepositories("com.pm.auth_service.repository")
 public class AuthServiceApplication {
